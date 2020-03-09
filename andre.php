6351cf65da5da2e722fd88446628ca162f4b5ab5
andreteam <61896976+andreteam@users.noreply.github.com>
2020-03-09 10:29:54 +0700

    andre

--- andre.php
+++ andre.php
@@ -5,8 +5,8 @@ echo color("green"," =================================== \n");
 echo color("green"," Claim Voucher \n");
 echo color("green"," Auto Create & Redeem Voucher \n");
 echo color("green"," =================================== \n");
-echo " Created by : Vey \n";
-echo " Version    : 555 \n";
+echo " Created by : andre \n";
+echo " Version    : 5 maret \n";
 echo " Time       : ".date('d-m-Y||H:i:s')." \n";
 echo color("green"," =================================== \n");
 
